4383b01bb2c76e77e550f7b876991854ca64e08c
Bea <bea.steers@gmail.com>
2022-06-07 14:20:46 -0400

    cleanup holoframe parser

--- ptgctl/holoframe.py
+++ ptgctl/holoframe.py
@@ -14,7 +14,7 @@ import cv2
 
 
 
-def load_all(read_data, dt_tol=0.1):
+def load_all(read_data):#, dt_tol=0.1
     '''Take data read from the API and convert them into hololens frames.
 
     Arguments:
@@ -23,6 +23,7 @@ def load_all(read_data, dt_tol=0.1):
     time_steps = defaultdict(dict)
     for stream_id, ts, data_bytes in read_data:
         time_steps[stream_id].update(load(data_bytes))
+        time_steps[stream_id]['timestamp'] = ts
 
     # time_steps = defaultdict(lambda: defaultdict(dict))
     # for stream_id, ts, data_bytes in read_data:
@@ -39,25 +40,25 @@ def unpack(data, keys):
     return [nested_key(data, k) for k in keys]
 
 
-def nested_key(data, key):
+def nested_key(data, key, default=...):
     d = data
     try:
         for ki in key.split('.'):
-            ki = int(ki) if isinstance(d, list) else ki
-            # print(ki, set(d))
-            d = d[ki]
-            # print(type(d))
+            d = d[int(ki) if isinstance(d, list) else ki]
         return d
     except KeyError:
+        if default != ...:
+            return default
         raise KeyError(f'{key!r} - {ki!r} not in {set(d) if isinstance(d, dict) else len(d) if isinstance(d, (list, tuple)) else d}')
 
 
 def get_image(data):
     '''Given a loaded dict of hololens data, access an image instance.'''
-    for k in ['main', 'glf', 'grf', 'glr', 'grr']:
-        im = data.get(k)
-        if im is not None:
-            return im
+    return next((
+        data[k]['image']
+        for k in ['main', 'glf', 'grf', 'glr', 'grr', 'depthlt']
+        if k in data
+    ), None)
 
 
 class SensorType:
@@ -79,22 +80,6 @@ class SensorType:
     Eye = 34
 
 
-SensorStreamMap = {
-    SensorType.PV: 'camera',
-    SensorType.DepthLT: 'depthlt',
-    SensorType.DepthAHAT: 'depthahat',
-    SensorType.GLL: 'camlr', 
-    SensorType.GLF: 'camlf',
-    SensorType.GRF: 'camrf',
-    SensorType.GRR: 'camrr',
-    SensorType.Accel: 'accel',
-    SensorType.Gyro: 'gyro',
-    SensorType.Mag: 'mag',
-    SensorType.numSensor: 'numsensor',
-    SensorType.Calibration: 'calibration',
-}
-
-
 
 header_dtype = np.dtype([
     ('version', np.uint8), 
@@ -110,117 +95,149 @@ header2_dtype = np.dtype([
 depth_dtype = np.dtype(np.uint16).newbyteorder('>')
 
 
-def load(data):
+def load(data, metadata=False, only_header=False):
     '''Parse any frame of data coming from the hololens.'''
-    full_data = data
-    data = memoryview(data)
+    parse = ByteParser(data)
+    read = not metadata  # disable reading images
 
-    (version, ftype), data = np_pop(data, header_dtype)
+    version, ftype = parse.pop(header_dtype)
+    d = dict(frame_type=ftype)
 
     # special cases
 
-    if ftype in {34}: # hand+eye - no header
-        return dict(orjson.loads(full_data.decode('ascii')), frame_type=ftype)
+    # hand+eye - no header
+    if ftype in {34}: 
+        if only_header:
+            return d
+        d.update(parse.reset().pop_json())
+        return d
+    # microphone
     if ftype == 172:
-        data = full_data
-        meta, data = split(data, 16)
-        sr, channels, pos = struct.unpack('<iiq', meta)
-        samples = np.frombuffer(data, dtype=np.float32).reshape((-1, channels))
-        return dict(audio=samples, sr=sr, pos=pos)
+        d['sr'], channels, d['pos'] = parse.reset().unpack('<iiq')
+        if only_header:
+            return d
+        d['audio'] = parse.pop(np.float32, (-1, channels), read=read)
+        return d
 
-    (ts, w, h, stride, info_size), data = np_pop(data, header2_dtype)
+    ts, w, h, stride, info_size = parse.pop(header2_dtype)
     im_size = w*h*stride
+    d['time'] = ts
+
+    if only_header:
+        return d
 
     # image
-
     if ftype in {SensorType.PV, SensorType.GLF, SensorType.GRR, SensorType.GRF, SensorType.GLL}:
-        im, data = split(data, im_size)
-        im = np.array(Image.frombytes('L', (w, h), bytes(im)))
-
-        if ftype in {SensorType.PV}:
-            im = cv2.cvtColor(im[:,:-8], cv2.COLOR_YUV2RGB_NV12)
-            im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-        elif ftype in {SensorType.GLF, SensorType.GRR}:
-            im = np.rot90(im, -1)
-        elif ftype in {SensorType.GRF, SensorType.GLL}:
-            im = np.rot90(im)
-        d = dict(frame_type=ftype, image=im)
-            
-        rig2world = focal = None
+        yuv = ftype in {SensorType.PV}
+        rot = -1 if ftype in {SensorType.GLF, SensorType.GRR} else 1 if ftype in {SensorType.GRF, SensorType.GLL} else 0
+        d['image'] = parse.pop_image(w, h, stride, yuv=yuv, rot=rot, read=read)
+    
         if info_size > 0:
-            x2world, data = np_pop(data, np.float32, (4,4))
-            x2world = x2world.T
-            d.update({'cam2world' if ftype in {SensorType.PV} else 'rig2world': x2world})
+            d['cam2world' if ftype in {SensorType.PV} else 'rig2world'] = parse.pop(np.float32, (4,4), T=True)
             if ftype in {SensorType.PV}:
-                focal, data = np_pop(data, np.float32, (2,))
-                principal_pt, data = np_pop(data, np.float32, (2,))
-                d.update(dict(focal=focal, principal_point=principal_pt))
-
+                d['focalX'], d['focalY'] = parse.pop(np.float32, (2,))
+                d['principalX'], d['principalY'] = parse.pop(np.float32, (2,))
         return d
 
     # depth
-
     if ftype in {SensorType.DepthLT, SensorType.DepthAHAT}:
-        depth, data = np_pop(data, depth_dtype, (h, w), im_size)
-        d = dict(frame_type=ftype, depth=depth)
-
-        ab = rig2world = None
+        d['image'] = parse.pop(depth_dtype, (h, w), read=read)
         if info_size >= im_size:
             info_size -= im_size
-            ab, data = np_pop(data, np.uint16, (h, w), im_size)
-            d.update(dict(infrared=ab))
-        
+            d['infrared'] = parse.pop(np.uint16, (h, w), read=read)
         if info_size > 0:
-            rig2world, data = np_pop(data, np.float32, (4,4))
-            rig2world = rig2world.T
-            d.update(dict(rig2world=rig2world))
-        
+            d['rig2world'] = parse.pop(np.float32, (4,4), T=True)
         return d
 
     # sensors
-
     if ftype in {SensorType.Accel, SensorType.Gyro, SensorType.Mag}:
-        sensorData, data = np_pop(data, np.float32, (h, w), im_size)
-        timestamps, data = np_pop(data, np.uint64, size=info_size)
-        timestamps = (timestamps - timestamps[0]) // 100 + ts
-        return dict(
-            frame_type=ftype,
-            **({SensorStreamMap[ftype]: sensorData}),
-            timestamps=timestamps)
+        d['data'] = parse.pop(np.float32, (h, w), im_size, read=read)
+        timestamps = parse.pop(np.uint64, size=info_size)
+        d['timestamps'] = (timestamps - timestamps[0]) // 100 + ts
+        return d
 
     # calibration
-
     if ftype in {SensorType.Calibration}:
-        # assert stride == 4  # just checking
-        lut, data = np_pop(data, np.float32, (w * h, 3))
-        rig2cam, data = np_pop(data, np.float32, (4,4))
-        rig2cam = rig2cam.T
-        return dict(frame_type=ftype, lut=lut, rig2cam=rig2cam)
+        d['lut'] = parse.pop(np.float32, (w * h, 3), read=read)
+        d['rig2cam'] = parse.pop(np.float32, (4,4), T=True)
+        return d
 
     raise ValueError(f"unknown frame type: {ftype}")
 
 
+class ByteParser:
+    def __init__(self, data):
+        self.data = memoryview(data)
+        self.offset = 0
 
+    @property
+    def remaining(self):
+        return len(self.data) - self.offset
 
-def np_read(data: bytes, dtype, shape=None):
-    '''Reads a numpy array of type and shape from the start of a byte array.'''
-    x = np.frombuffer(data, dtype)
-    return x.reshape(shape) if shape else x.item() if x.size == 1 else x
+    @property
+    def total(self):
+        return len(self.data)
 
-def split(data, l):
-    '''split an array at an index.'''
-    return data[:l], data[l:]
+    def _pop_bytes(self, size):
+        if size is None:  # if None, go to the end
+            size = len(self.data) - self.offset
+        i, j = self.offset, self.offset + size
+        self.offset = j
+        return self.data[i:j]
+
+    def _read_np(self, x, dtype, shape=None, T=False):
+        x = np.frombuffer(x, dtype)
+        if shape:
+            x = x.reshape(shape)
+        elif x.size == 1:
+            x = x.item()
+        if T:
+            x = x.T
+        return x
+
+    def _read_image(self, im, w, h, rot=0, yuv=False):
+        im = np.array(Image.frombytes('L', (w, h), im))
+        if yuv:
+            im = cv2.cvtColor(im[:,:-8], cv2.COLOR_YUV2RGB_NV12)
+        if rot:
+            im = np.rot90(im, rot)
+        return im
+
+    def _read_json(self, x):
+        return orjson.loads(x.decode('ascii'))
+
+    def pop(self, dtype, shape=None, size=None, read=True, **kw):
+        x = self._pop_bytes(size or np_size(dtype, shape))
+        if read:
+            x = self._read_np(x, dtype, shape, **kw)
+        return x
+
+    def pop_image(self, w, h, stride, read=True, **kw):
+        x = self._pop_bytes(w * h * stride)
+        if read:
+            x = self._read_image(x, w, h, **kw)
+        return x
+
+    def pop_json(self, size=None, read=True):
+        x = self._pop_bytes(size)
+        if read:
+            x = self._read_json(x)
+        return x
+
+    def unpack(self, format):
+        data = self._pop_bytes(struct.calcsize(format))
+        return struct.unpack('<iiq', data)
+
+    def reset(self):
+        self.offset = 0
+        return self
+
 
 def np_size(dtype, shape=None):
     '''Get the size of an array with data type and shape.'''
     mult = 1
     for s in shape or ():
         if s < 0:
-            raise ValueError("Can't get absolute size for a flexible shape array.")
+            return None
         mult *= s
     return np.dtype(dtype).itemsize * mult
-
-def np_pop(data, dtype, shape=None, size=None):
-    '''Read a numpy array from a byte array and chop them from the start of the array.'''
-    x, leftover = split(data, size or np_size(dtype, shape))
-    return np_read(x, dtype, shape), leftover
