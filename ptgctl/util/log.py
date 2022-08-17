b61ac9b5790257b5d610f205a4109ce0bb65a729
Bea Steers <bea.steers@gmail.com>
2022-08-17 15:25:40 -0400

    fix colors

--- ptgctl/util/log.py
+++ ptgctl/util/log.py
@@ -3,30 +3,30 @@ import logging
 
 
 NC='\033[0m'
-def color(text, color='grey', brightness=0):
-    return f'{color_code(color, brightness)}{text}{NC}'
+def color(text, color='white', bright=0, intense=0, bg=0):
+    return f'{color_code(color, bright, 60*bool(intense) + 10*bool(bg))}{text}{NC}'
 
-def color_code(color='grey', brightness=0):
-    i = 1 if brightness > 0 else 2 if brightness < 0 else 0
-    c = COLOR_CODES[color.lower()]
+def color_code(color='grey', bright=0, offset=0):
+    i = 1 if bright > 0 else 2 if bright < 0 else 0
+    c = COLOR_CODES[color.lower()]+offset
     return f'\033[{i};{c}m'
 
 COLOR_CODES = {
-    'red':    30,
-    'orange': 31,
+    'black':  30,
+    'red':    31,
     'green':  32,
     'yellow': 33,
     'blue':   34,
     'purple': 35,
     'cyan':   36,
-    'grey':   37,
+    'white':  37,
 }
 
 class ColorFormatter(logging.Formatter):
     LEVELS = {
         logging.DEBUG: color_code('purple', 0), 
-        logging.INFO: color_code('grey', -1), 
-        logging.WARNING: color_code('orange', 1), 
+        logging.INFO: color_code('white', -1), 
+        logging.WARNING: color_code('yellow', 1), 
         logging.ERROR: color_code('red', 0), 
         logging.CRITICAL: color_code('red', 1), 
     }
@@ -58,3 +58,15 @@ def aslevel(level):
     return logging._nameToLevel.get(
         level.upper() if isinstance(level, str) and level not in logging._nameToLevel else level, 
         level)
+
+
+def show_colors(*colors):
+    
+    for c in colors or COLOR_CODES:
+        for b in [0, 1]:
+            print(f'bright={b}', '\t'.join([color(f'{c}', c, b, i, bg) for i in [0,1] for bg in [0,1]]))
+
+
+if __name__ == '__main__':
+    import fire
+    fire.Fire()
