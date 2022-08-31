811d0e34a6bdcde64fc423ab956038b636fb49f6
Jianzhe Lin <jianzhelin@gmail.com>
2022-08-31 15:25:49 -0400

    Update re_id.py

--- examples/re_id.py
+++ examples/re_id.py
@@ -42,8 +42,19 @@ class ReId:
         # get boxes with xyz coords and filter bad matches
         boxes_xyz_world, dist = pts3d.transform_box(boxes, return_corners=False)
         valid = dist < self.MIN_DEPTH_POINT_DISTANCE
-        boxes = boxes[valid]
-        boxes_xyz_world = boxes_xyz_world[valid]
+#        boxes = boxes[valid]
+#        boxes_xyz_world = boxes_xyz_world[valid]
+        for item in range(len(boxes)):
+            if int(boxes[item][5]) == 61 or int(boxes[item][5]) == 4 or int(boxes[item][5]) == 62 or int(boxes[item][5]) == 29:
+                boxes[item][5] = 45
+            elif int(boxes[item][5]) == 65 or int(boxes[item][5]) == 27 or int(boxes[item][5]) == 74 or int(boxes[item][5]) == 79 or int(boxes[item][5]) == 67:
+                boxes[item][5] = 52 
+            elif int(boxes[item][5]) == 14 or int(boxes[item][5]) == 64:
+                boxes[item][5] = 0 
+            elif int(boxes[item][5]) == 59 or int(boxes[item][5]) == 28 or int(boxes[item][5]) == 63:
+                boxes[item][5] = 60  
+            elif int(boxes[item][5]) == 37:
+                boxes[item][5] = 43          
         labels = np.asarray(results.names)[boxes[:, 5].astype(int)]
 
         # compare boxes with previous instances
@@ -66,53 +77,55 @@ class ReId:
     def update_memory(self, xyz, label):
         # check memory
         for k, xyz_seen in self.location_memory.items():
-            if self.memory_comparison(xyz_seen, xyz):
+            if label == k and self.memory_comparison(xyz_seen, xyz):
                 return k, True
-
+            elif label == k[:-2] and self.memory_comparison(xyz_seen, xyz):
+                return k, True
         # unique name for multiple instances
         if label in self.location_memory:
             self.instance_count[label] += 1
             i = self.instance_count[label]
             label = f'{label}_{i}'
-
+        
         # TODO: add other info
         self.location_memory[label] = xyz
         return label, False
 
     def memory_comparison(self, seen, candidate):
         '''Compare a new instance to a previous one. Determine if they match.'''
-        return np.linalg.norm(candidate - seen) < 0.8
+        return np.linalg.norm(candidate - seen) < 2
 
 
 class DrawResults:
     def __init__(self, memory):
         self.location_memory = memory
 
-    def draw_4panel(self, results):
-        rgb = results.imgs[0]
+    def draw_4panel(self, results, rgb):
+        res = results.imgs[0]
         return np.vstack([
             np.hstack([
-                self.draw_memory_yolo(results), 
+                self.draw_memory_yolo(results, rgb), 
                 self.draw_basic_yolo(results),
             ]), 
             np.hstack([
-                self.draw_message_board(rgb.shape), 
-                self.draw_3d_space(rgb.shape),
+                self.draw_message_board(res.shape), 
+                self.draw_3d_space(res.shape),
             ]),
         ])
 
-    def draw_memory_yolo(self, results):
-        img = results.imgs[0]
+    def draw_memory_yolo(self, results, rgb):
+#        img = results.imgs[0]
         for b, seen in zip(results.xywh[0], results.seen_before):
-            img = draw_bbox(
-                img, *b[:4], 
+            rgb = draw_bbox(
+                rgb.copy(), *b[:4], 
                 color=(255, 0, 0) if seen else (0, 255, 0))
-        draw_text_list(img, [
+        draw_text_list(rgb, [
             f'hey I remember {name}'
             for name, seen in zip(results.track_ids, results.seen_before)
             if seen
         ])
-        return img
+        return rgb.copy()
+
 
     def draw_basic_yolo(self, results):
         return results.render()[0]
@@ -129,13 +142,13 @@ class DrawResults:
         fig = plt.figure()
         ax = plt.axes(projection='3d')
 
-        # ax.set_xlim((-5, 5))
-        # ax.set_ylim((-5, 5))
-        # ax.set_zlim((-5, 5))
+        # ax.set_xlim((-2, 0))
+        # ax.set_ylim((-2, 0))
+        # ax.set_zlim((-2, 0))
 
         for name, loc in self.location_memory.items():    
             ax.scatter(*loc, marker='^')   
-            ax.text(*loc, name, fontsize=10)
+            ax.text(*loc, name, fontsize=6)
 
         fig.canvas.draw()
         src = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
@@ -150,7 +163,7 @@ class DrawResults:
 
 def draw_text_list(img, texts):
     for i, txt in enumerate(texts):
-        cv2.putText(img, txt, (400, 200+30*i), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 48, 48), 2)
+        cv2.putText(img, txt, (20+400*(i//12), 40+30*(i%12)), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 48, 48), 2)
     return img
 
 
@@ -173,7 +186,7 @@ async def _main(**kw):
     i = 0
 
     api = ptgctl.CLI(local=False)
-    streams = ['main', 'depthlt']
+    streams = ['replay:main', 'replay:depthlt']
 
     data = ptgctl.holoframe.load_all(api.data('depthltCal'))
     lut, T_rig2cam = ptgctl.holoframe.unpack(data, [
@@ -182,6 +195,7 @@ async def _main(**kw):
     ])
 
     async with api.data_pull_connect('+'.join(streams), **kw) as ws:
+        indx = 0
         while True:
             data = await ws.recv_data()
             data = ptgctl.holoframe.load_all(data)
@@ -193,17 +207,19 @@ async def _main(**kw):
                 principalX, principalY,
             ) = ptgctl.holoframe.unpack(
                 data, [
-                'main.image', 
-                'depthlt.image', 
-                'depthlt.rig2world', 
-                'main.cam2world', 
-                'main.focalX', 
-                'main.focalY', 
-                'main.principalX',
-                'main.principalY',
+                'replay:main.image', 
+                'replay:depthlt.image', 
+                'replay:depthlt.rig2world', 
+                'replay:main.cam2world', 
+                'replay:main.focalX', 
+                'replay:main.focalY', 
+                'replay:main.principalX',
+                'replay:main.principalY',
             ])
             rgb = cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB)
-
+            cv2.imshow('aa', rgb)
+            indx += 1
+            cv2.imwrite('C:/codes/ptgctl/img_inp/'+ str(indx) + '.jpg', rgb)
             results = reid.process_frame(
                 rgb, depth, lut, 
                 T_rig2world, 
@@ -212,8 +228,9 @@ async def _main(**kw):
                 [focalX, focalY], 
                 [principalX, principalY]
             )
-            out_img = drawer.draw_4panel(results)
+            out_img = drawer.draw_4panel(results, rgb)
             cv2.imshow('main', out_img)
+            cv2.imwrite('C:/codes/ptgctl/saved_images/'+ str(indx) + '.jpg', out_img)    
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
             
