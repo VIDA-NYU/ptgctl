598b4a749f6f7cc46be9c4eef13fc780a3c15327
ptg <ptg@monster-machine>
2024-11-13 17:08:51 -0500

    fix websockets breaking change

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     entry_points={'console_scripts': ['{name}={name}:main'.format(name=NAME)]},
     install_requires=[
-        'requests', 'websockets', 'fire>=0.5.0',
+        'requests', 'websockets>=14.0.0', 'fire>=0.5.0',
         # 'fire @ git+ssh://git@github.com/google/python-fire@master#egg=fire', 
         'tabulate', 'tqdm', 'IPython',
         'redis_record>=0.0.4',
